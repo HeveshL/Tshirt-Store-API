81d67dd03b4f29fdcd3000c01e50c048635e2293
Hevesh <hevesh000@gmail.com>
2023-02-01 20:58:00 +0530

    Initial Commit

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectWithDb = () => {
+    mongoose
+        .connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(console.log("DB got connected"))
+        .catch((err) => {
+            console.log("DB connection issues");
+            console.log(err);
+            process.exit(1);
+        });
+};
+
+module.exports = connectWithDb;
